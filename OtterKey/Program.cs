1f9fde068fe0c0905db71958eb87b42378b80f05
Ronny Natsuo <1655659+natsuo@users.noreply.github.com>
2018-08-19 02:48:28 +0200

    Add project files.

---
+++ OtterKey/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OtterKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
